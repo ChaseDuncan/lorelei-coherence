75abf28f85e61622c500a88828d309b2cc94c896
Chase Duncan <cddunca2@bronte.cs.illinois.edu>
2017-08-04 13:30:07 -0500

    Adding new loreleiedl package.

---
+++ src/main/java/edu/illinois/cs/cogcomp/loreleiedl/CandidateList.java
@@ -0,0 +1,6 @@
+package edu.illinois.cs.cogcomp.loreleiedl;
+
+import java.util.ArrayList;
+import edu.illinois.cs.cogcomp.loreleiedl.KBEntity;
+
+public class CandidateList extends ArrayList<KBEntity>{}
---
+++ src/main/java/edu/illinois/cs/cogcomp/loreleiedl/MentionList.java
@@ -0,0 +1,6 @@
+package edu.illinois.cs.cogcomp.loreleiedl;
+
+import java.util.ArrayList;
+import edu.illinois.cs.cogcomp.loreleiedl.EDLMention;
+
+public class MentionList extends ArrayList<EDLMention>{}
